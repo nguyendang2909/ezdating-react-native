726e594cf44345bb4f51e20c3c411d747802f4d8
nguyendang2909 <nguyendang2909@gmail.com>
2023-12-03 01:24:14 +0700

    feat: update types

--- app/screens/Me/EditInfoHeightScreen.tsx
+++ app/screens/Me/EditInfoHeightScreen.tsx
@@ -1,8 +1,9 @@
 import { Picker } from '@react-native-picker/picker';
-import { useNavigation } from '@react-navigation/native';
 import { useUpdateProfileMutation } from 'app/api';
 import { HeaderSaveModal } from 'app/components/Header/HeaderSaveModal';
+import { SCREENS } from 'app/constants';
 import { useAppSelector, useMessages } from 'app/hooks';
+import { goBack } from 'app/navigators';
 import { notificationsService } from 'app/services/notifications/notifications.service';
 import { useFormik } from 'formik';
 import { Box, Text, View } from 'native-base';
@@ -12,7 +13,6 @@ import * as Yup from 'yup';
 export const EditInfoHeightScreen = () => {
   const { formatMessage } = useMessages();
   const [updateProfile] = useUpdateProfileMutation();
-  const { goBack } = useNavigation();
   const currentHeight = useAppSelector(state => state.app.profile?.height);
 
   const formik = useFormik<{ height: number }>({
@@ -28,7 +28,7 @@ export const EditInfoHeightScreen = () => {
       try {
         await updateProfile(values).unwrap();
         notificationsService.updateSuccess();
-        goBack();
+        goBack(SCREENS.ProfileEdit);
       } catch (err) {
         notificationsService.updateFail();
       }
@@ -52,7 +52,7 @@ export const EditInfoHeightScreen = () => {
         >
           {Array.from({ length: 100 }, (value, index) => {
             const heightValue = index + 100;
-            return <Picker.Item label={heightValue.toString()} value={heightValue} />;
+            return <Picker.Item key={index} label={heightValue.toString()} value={heightValue} />;
           })}
         </Picker>
       </View>
