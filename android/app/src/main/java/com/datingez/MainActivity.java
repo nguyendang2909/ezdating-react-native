eff7c0a70b85b28eccf739d03d3b30d3e90e4c52
nguyendang2909 <nguyendang2909@gmail.com>
2023-12-17 17:50:49 +0700

    feat: rename app

--- android/app/src/main/java/com/datingez/MainActivity.java
+++ android/app/src/main/java/com/datingez/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ezdating;
+package com.datingez;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "EZDating";
+    return "DatingEZ";
   }
 
   /**
--- ios/DatingEZTests/DatingEZTests.m
+++ ios/DatingEZTests/DatingEZTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface EZDatingTests : XCTestCase
+@interface DatingEZTests : XCTestCase
 
 @end
 
-@implementation EZDatingTests
+@implementation DatingEZTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
